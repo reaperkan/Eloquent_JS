37ec2204b6f4bf2f8d41bd0e30fb278de70fc1ad
reaperkan <ksengupta100@gmail.com>
2023-12-03 16:14:45 +0530

    Chapter 7 half exercises

--- EloquentJS/Chapter_7/Robot.js
+++ EloquentJS/Chapter_7/Robot.js
@@ -53,3 +53,98 @@ let next = first.move("Alice's House");
 console.log(next.place);
 console.log(next.parcels);
 console.log(first.place);
+
+function runRobot(state, robot, memory){
+    for(let turn = 0;; turn++){
+        if(state.parcels.length == 0){
+            console.log(`Done in ${turn} turns`);
+            return turn;
+        }
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+        console.log(`Moved to ${action.direction}`);
+    }
+}
+
+function randomPick(array){
+    let choice = Math.floor(Math.random() * array.length);
+    return array[choice];
+}
+
+function randomRobot(state){
+    return { direction: randomPick(roadGraph[state.place])};
+}
+
+VillageState.random = function(parcelCount = 5){
+    let parcels = [];
+    for(let i =0; i < parcelCount; i++){
+        let address = randomPick(Object.keys(roadGraph));
+        let place;
+        do {
+            place = randomPick(Object.keys(roadGraph));
+        }while(place == address);
+        parcels.push({ place: place, address: address});
+    }
+    return new VillageState("Post Office", parcels);
+}
+
+//runRobot(VillageState.random(), randomRobot);
+
+const mailRoute = [
+    "Alice's House", "Cabin", "Alice's House", "Bob's House", "Town Hall", "Daria's House",
+    "Ernie's House", "Grete's House", "Shop", "Grete's House", "Farm", "Marketplace", "Post Office"
+]
+
+function routeRobot(state, memory = []){
+    if(memory.length == 0){
+        memory = mailRoute;
+    }
+
+    return { direction: memory[0], memory: memory.slice(1)};
+} 
+
+//runRobot(VillageState.random(), routeRobot);
+
+function findRoute(graph, from, to){
+    let work = [ {at: from, route: []} ];
+    for(let i = 0; i < work.length; i ++){
+        let {at, route} = work[i];
+        for(let place of graph[at]){
+            if(place == to)
+                return route.concat(place);
+            if(!work.some(w => w.at == place)){
+                work.push({at: place, route: route.concat(place)});
+            }
+        }
+    }
+}
+
+function goalOrientedRobot({place, parcels}, route = []){
+    if(route.length == 0){
+        let parcel = parcels[0];
+        if(parcel.place != place){
+            route = findRoute(roadGraph, place, parcel.place);
+        }else{
+            route = findRoute(roadGraph, place, parcel.address);
+        }
+    }
+
+    return {direction: route[0], memory: route.splice(1)};
+}
+
+//runRobot(VillageState.random(), goalOrientedRobot);
+
+function compareRobot(robotA, robotB){
+    let robotA_turns = 0, robotB_turns = 0;
+    for(let i = 0; i < 100; i++){
+        const initialState = VillageState.random();
+        robotA_turns += runRobot(initialState, robotA);
+        robotB_turns += runRobot(initialState, robotB);
+    }
+
+    console.log(`Robot A on average took ${robotA_turns / 100}`);
+    console.log(`Robot B on average took ${robotB_turns / 100}`);
+}
+
+compareRobot(goalOrientedRobot, routeRobot);
