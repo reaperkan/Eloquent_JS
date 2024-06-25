ef13eacec91f4bdd8171baf1d6d7e865a8555e5e
reaperkan <ksengupta100@gmail.com>
2024-06-26 00:47:55 +0530

    Chapter 10 exercise

---
+++ EloquentJS/Chapter_10/graph.js
@@ -0,0 +1,15 @@
+module.exports = function buildGraphs(edges) {
+  const graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+  for (const [from, to] of edges) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+};
